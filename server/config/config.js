ed8258357bf474ea5418f153183a661a6b522a08
Edu Franco <eduardo.ezequiel.franco@gmail.com>
2019-12-10 17:44:22 -0300

    Variable de entorno MONGO_URI

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlBD;
 if (process.env.NODE_ENV === 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://eduardo:Lalo353075502247@cluster0-y3juj.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URI;
 }
 process.env.URLBD = urlBD;
